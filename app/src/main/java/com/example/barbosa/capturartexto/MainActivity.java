72fc9ca233ea4d912b1602633ad371ae71fdc1a1
barbosalano <personaltecnologiacg@gmail.com>
2018-10-30 08:28:55 -0300

    Capturação de texto de um TextInputLayout e colocar em um TextEdit

---
+++ app/src/main/java/com/example/barbosa/capturartexto/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.barbosa.capturartexto;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   private TextInputEditText campoProduto;
+   private TextView resultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campoProduto = findViewById(R.id.etNomeProduto);
+        resultado = findViewById(R.id.tvResultado);
+    }
+
+    public void btnEnviar(View view){
+
+        String nomeProduto = campoProduto.getText().toString();
+        resultado.setText(nomeProduto);
+
+    }
+}
